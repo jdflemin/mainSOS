374d86dfb6912783f7b6a1e84c3d3ffd282ebaba
jdflemin <jdf@mchsi.com>
2017-05-18 10:08:32 -0700

    added uifunctions

---
+++ ngApp/UIfunctions/UIfunctions.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('#dropDownMenu').click(function(){
+    $('#dropDownContents').slideToggle(500)
+  })
+})
+
+$(function(){                   //SHOULD THIS BE MOVED TO A CONTROLLER?
+  $('.replyButton').click(function(){
+    $('.discussionContainer').slideToggle(500)
+  })
+})
