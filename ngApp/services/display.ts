c7238c786b716c2f770b51ace40c991945eced32
jdflemin <jdf@mchsi.com>
2017-05-17 18:57:22 -0700

    added services

--- ngApp/services/display.ts
+++
@@ -1,8 +0,0 @@
-namespace mainsos.Service {
-
-  class DisplayService {
-
-
-  }
-  angular.module('mainsos').service('displayService', DisplayService);
-}
--- ngApp/services/search.ts
+++
@@ -1,8 +0,0 @@
-namespace mainsos.Service {
-
-  class SearchService {
-
-
-  }
-  angular.module('mainsos').service('searchService', SearchService);
-}
