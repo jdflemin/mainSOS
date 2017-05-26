9c57928152bb88542bc224794996bca88533fae8
LowRaine <lornibut@hotmail.com>
2017-05-25 17:11:29 -0700

    Update lessonController.ts
    added lessonController to delete, edit, and add.

--- ngApp/controllers/lessonController.ts
+++ ngApp/controllers/lessonController.ts
@@ -1,6 +1,7 @@
-namespace mainsos.Controllers{
+namespace SOS.Controllers{
 
   export class LessonsController {
+    public lesson;
     public lessons;
     public course;
 
@@ -14,5 +15,25 @@ namespace mainsos.Controllers{
     public listLessons(){
       this.lessons = this.lessonServices.getAllCourseLessons(this.course._id);
     }
+
+    public delete(lesson){
+      this.lessonServices.delete(lesson)
+      .then((data)=> {
+        this.lessons = this.lessonServices.listLessons();
+      });
+    }
+
+    public edit(lesson){
+      this.lessonServices.edit(lesson);
+    }
+
+    public add(lesson){
+      let newLesson = {courseId:'', title:''};
+      newLesson.courseId= this.lesson.courseId;
+      newLesson.title= this.lesson.title;
+      this.lesson.unshift(newLesson);
+      this.lesson.courseId='';
+      this.lesson.title='';
+      }
   }
 }
