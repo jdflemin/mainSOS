45e79efbd1a8b4d3cd89d3f67421c5fc98d2b718
Rosa Andrews <rosamaya.27@hotmail.com>
2017-05-30 16:29:43 -0700

    testing

--- ngApp/controllers/questionController.ts
+++ ngApp/controllers/questionController.ts
@@ -1,34 +1,36 @@
 namespace mainsos.Controllers {
 
-  export class QuestionsController {
+  export class QuestionController {
     private lesson;
-    public questions;
-    public question;
+    private questions;
+    //private question;
     public newQuestion = {
       qTitle: '',
       qContent: '',
       qDate: Date.now(),
-      lessonID: this.lesson._id,
+      lessonID: '',
 
     }
     public clickCount = 0;
     public userId= '';
 
 
-    constructor(private questionService, private lessonServices, private $state) {
-      this.lessonServices.getOne($state.params.id).then((data) => {
+    constructor(private lessonServices, private questionService, private $stateParams, private $state) {
+      console.log($stateParams.id);
+      lessonServices.getOne($stateParams.id).then((data) => {
           this.lesson = data;
+          this.listQuestions();
         })
-        this.listQuestions();
     }
 
     public listQuestions() {
+      console.log(this.lesson);
       this.questions = this.questionService.getAllByLesson(this.lesson._id);
     }
 
     public redirectToAnswers(question) {
-      console.log(this.question._id);
-      this.$state.go('answers', {id: this.question._id});
+      console.log(question._id);
+      this.$state.go('answers', {id: question._title});
     }
 
     public addQuestions(question) {
