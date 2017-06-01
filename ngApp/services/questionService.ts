a330a405a32736676ad4d3c72b84f4330a7293d0
Andrews27 <RosaMayA.27@Hotmail.com>
2017-05-31 18:03:30 -0700

    Update questionService.ts

--- ngApp/services/questionService.ts
+++ ngApp/services/questionService.ts
@@ -1,17 +1,10 @@
 namespace mainsos.Services{
 
   export class QuestionService {
-<<<<<<< HEAD
     private QUESTION_RESOURCE;
     private LESSON_QUESTION_RESOURCE;
     private SEARCH_RESOURCE;
     private CRUD_QUESTION_RESOURCE;
-=======
-    private QUESTION_RESOURCE = this.$resource('http://localhost:3002/api/v1/questions/:id');
-    private LESSON_QUESTION_RESOURCE = this.$resource('http://localhost:3002/api/v1/questions/lessons/:id/questions');
-    private SEARCH_RESOURCE = this.$resource('http://localhost:3001/api/v1/questions/search/:search');
-    private CRUD_QUESTION_RESOURCE = this.$resource('http://localhost:3003/api/v1/questions/:id')
->>>>>>> refs/remotes/origin/master
 
     constructor(private $resource) {
       this.QUESTION_RESOURCE = this.$resource('http://localhost:3002/api/v1/questions/:id');
