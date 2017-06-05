4dc165406f71f79605db681fb98c3fbc38eba260
DESKTOP-O8MO24O\angad <angadreme@gmail.com>
2017-06-05 12:29:17 -0700

    working search

--- ngApp/controllers/searchController.ts
+++ ngApp/controllers/searchController.ts
@@ -1,23 +1,54 @@
 namespace mainsos.Controllers{
 
   export class SearchController{
-    private questions;
+    private questions = [];
+    private answers;
     private searchString;
 
-    constructor(private questionService, private answerService, private $stateParams) {
-      this.searchString = "test"; //hard coded to test for testing purooses. need to ask justin about the ng-model on indexcontroller and index.ejs (set this to $stateParams.search when passing correctly)
+    constructor(private questionService, private answerService, private $stateParams, private $state) {
+      this.searchString = $stateParams.search;
       this.searchQuestionContent();
+      this.searchAnswers();
       this.searchString = '';
     }
 
     public searchAgain(){
       this.searchQuestionContent();
+      this.searchAnswers();
+      this.searchString = '';
     }
 
     public searchQuestionContent() {
       this.questions = this.questionService.searchQuestionContent(this.searchString);
     }
 
+  public searchAnswers() {
+       this.answerService.searchAnswerContent(this.searchString).then((data) => {
+         this.answers = data;
+         this.addtoQuestions();
+       });
+    }
+
+    public addtoQuestions() {
+      for(let i = 0; i < this.answers.length; i++){
+        this.questionService.getOne(this.answers[i].questionId).then((data) => {
+          let tempQuestion = data;
+          let pushThis = true;
+          for(let j = 0; j < this.questions.length; j++) {
+            if(this.questions[j]._id == tempQuestion._id){
+              pushThis = false;
+            }
+          }
+          if(pushThis){
+            this.questions.push(tempQuestion);
+          }
+        });
+      }
+    }
+
+    public redirectToAnswers(questionID) {
+      this.$state.go('answers', {id: questionID});
+    }
 
   }
   angular.module('mainsos').controller('searchController', SearchController);
